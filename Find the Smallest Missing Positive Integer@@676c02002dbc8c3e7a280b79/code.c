357cea4173f3628dbef3cc67ece61d2771a8c500
Madhav Sakhuja <msakhuja22@gmail.com>
2025-03-27 20:39:29 +0530

    Solution to Find the Smallest Missing Positive Integer

--- Find the Smallest Missing Positive Integer@@676c02002dbc8c3e7a280b79/code.c
+++ Find the Smallest Missing Positive Integer@@676c02002dbc8c3e7a280b79/code.c
@@ -1,30 +1,37 @@
 #include<stdio.h>
-void sort(int arr[],int n){
-    for(int i=0;i<n-1;i++){
-        for(int j=0;j<n-i-1;j++){
-            if(arr[j]>arr[j+1]){
-                int temp=arr[j];
-                arr[j]=arr[j+1];
-                arr[j+1]=temp;
+
+void sort(int arr[], int n) {
+    for(int i = 0; i < n - 1; i++) {
+        for(int j = 0; j < n - i - 1; j++) {
+            if(arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
             }
         }
     }
 }
-int main(){
+
+int main() {
     int n;
-    scanf("%d",&n);
+    scanf("%d", &n);
     int arr[n];
-    for(int i=0;i<n;i++){
-        scanf("%d",&arr[i]);
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
     }
-    sort(arr,n);
-    for(int i=0;i<n;i++){
-        if(arr[i+1]==arr[i]+1){
-            continue;
+
+    sort(arr, n);
+
+    int missing = 1;
+    for(int i = 0; i < n; i++) {
+        if (arr[i] < 0) {
+            continue; 
         }
-        else{
-            printf("%d",arr[i]+1);
-            return 0;
+        if (arr[i] == missing) {
+            missing++;
         }
     }
+    printf("%d\n", missing);
+
+    return 0;
 }
